e9175659354238bf461484e41f5ba209c8f31f08
christopher-codoxx <christopher.santana@codoxx.com>
2022-05-05 01:23:56 -0400

    Made built

--- .nuxt/layouts/default.vue
+++ .nuxt/layouts/default.vue
@@ -1,22 +1,3 @@
 <template>
-  <div>
-    <HeaderComponent></HeaderComponent>
-    <Subheader></Subheader>
   <Nuxt />
-  </div>
 </template>
-
-
-<script lang="ts">
-import Vue from 'vue'
-import HeaderComponent from '@/components/header-component.vue'
-import Subheader from '@/components/subheader.vue'
-export default Vue.extend({
-
-  components: {
-    HeaderComponent,
-    Subheader
-  }
-  
-})
-</script>
